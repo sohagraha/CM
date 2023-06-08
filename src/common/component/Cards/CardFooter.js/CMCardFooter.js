e1009505be5bc85d7272d76829005987723c00b5
sohagraha <sohagrahabd@gmail.com>
2023-06-08 11:39:11 +0600

    card complete

--- src/common/component/Cards/CardFooter.js/CMCardFooter.js
+++ src/common/component/Cards/CardFooter.js/CMCardFooter.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./CMCardFooter.scss";
+import moment from "moment/moment";
 
 const CMCardFooter = ({ date }) => {
-  return <div className="cmCardFooter">Last updated: {Date.now()}</div>;
+  return (
+    <div className="cmCardFooter">
+      Last updated : {moment(date).format("MMM DD, YYYY @ hh:mm A")}
+    </div>
+  );
 };
 
 export default CMCardFooter;
